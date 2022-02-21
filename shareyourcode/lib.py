91810e41b3b90dfe5ca429bcded4839fda89dfff
christophe lanson <christophe.lanson119@orange.fr>
2022-02-21 16:48:54 +0100

    with a test

--- shareyourcode/lib.py
+++ shareyourcode/lib.py
@@ -1,7 +1,6 @@
-
 def try_me():
     print('go back to work you lasy arse')
+    return 'message printed'
 
 if __name__ == '__main__':
     try_me()
-    
---
+++ tests/lib_test.py
@@ -0,0 +1,7 @@
+from shareyourcode.lib import try_me
+
+def test_string_length():
+    assert try_me() == 'message printed'
+
+if __name__ == '__main__':
+    test_string_length()
